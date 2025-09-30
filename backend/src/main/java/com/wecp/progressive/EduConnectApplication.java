4d10970b57e9be19ceaadb1ef4076e8d15955ec0
Krishna Chaitanya Reddy Seelam <166292691+KrishnaChaituReddy@users.noreply.github.com>
2025-09-30 22:43:00 +0530

    Delete backend directory

--- backend/src/main/java/com/wecp/progressive/EduConnectApplication.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive;
-
-public class EduConnectApplication {
-    public static void main(String[] args) {
-        System.out.println("Welcome to EduConnect Progressive Project!");
-    }
-}
