22607220d4710d0cffaf1a2f85d57e1b3925bf82
username <wecp-dummy@gmail.com>
2025-10-04 17:57:19 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/UserLoginServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/UserLoginServiceImpl.java
@@ -1,23 +1,65 @@
 package com.wecp.progressive.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.wecp.progressive.dto.UserRegistrationDTO;
 import com.wecp.progressive.entity.User;
+import com.wecp.progressive.jwt.JwtUtil;
+import com.wecp.progressive.repository.UserRepository;
 
-public class UserLoginServiceImpl {
+@Service
+public class UserLoginServiceImpl implements UserDetailsService {
 
-    User createUser(User user) {
-        return null;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+   public  User createUser(User user) {
+     return userRepository.save(user);
     }
 
-    User getUserByUsername(String username) {
-        return null;
+   public  User getUserByUsername(String username) {
+    return userRepository.findByUsername(username);
     }
 
-    void registerUser(UserRegistrationDTO userRegistrationDTO) {
+   public  void registerUser(UserRegistrationDTO userRegistrationDTO) throws Exception {
+    if(userRepository.findByUsername(userRegistrationDTO.getUsername())!=null)
+    {
+      throw new Exception("user alredy there");
+    }
+    User user=new User();
+    user.setUsername(userRegistrationDTO.getUsername());
+    user.setRole(userRegistrationDTO.getRole());
+    user.setPassword(passwordEncoder.encode(userRegistrationDTO.getPassword()));
+  userRepository.save(user);
+       
 
     }
 
-    User getUserDetails(int userId) {
-        return null;
+  public   User getUserDetails(int userId)  {
+  if(userRepository.findById(userId).isEmpty())
+  {
+    throw new UsernameNotFoundException("User not found with ID: "+ userId);
+  }
+  return  userRepository.findById(userId).get();
     }
+    public UserDetails loadUserByUsername(String username)
+    {
+       User user=userRepository.findByUsername(username);
+       if(user==null)
+       {
+        throw new UsernameNotFoundException("user not found exception");
+       }
+       return org.springframework.security.core.userdetails.User
+       .withUsername(user.getUsername())
+       .password(user.getPassword())
+       .roles(user.getRole())
+       .build();
+    }
+
 }
