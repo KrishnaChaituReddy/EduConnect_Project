3cb3e0303ee83209d051033bdec2271938003728
username <wecp-dummy@gmail.com>
2025-10-07 13:00:13 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/UserRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.wecp.progressive.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+import com.wecp.progressive.entity.User;
+
+public interface UserRepository extends JpaRepository<User,Integer>{
+
+    public User findByUsername(String username);
+    // public User findByTeacherId(int teacherId);
+    // public User findByStudentId(int studentId);
+    // public void deleteByTeacher(int teacherId);
+    // public void deleteByStudentId(int studentId);
 }
--- frontend/day_14_15/js/validation.js
+++ frontend/day_14_15/js/validation.js
@@ -2,12 +2,56 @@ function login() {
    
     // You can perform login validation and authentication here
     // For simplicity, let's just display an alert
+
+    const username=document.getElementById("loginUsername").value;
+    const password=document.getElementById("loginPassword").value;
+    const message=document.getElementById("message");
+    if(!username || !password)
+    {
+        message.innerHTML="All fields are requried";
+    }
+    console.log(`Login clicked. Username: ${username}, Password: ${password}`);
     
 }
 
 function register() {
-   
+ 
+    //const name=document.getElementById("registerName");
+    const name=document.getElementById("registerName").value;
+    const email=document.getElementById("registerEmail").value;
+    const username=document.getElementById("registerUsername").value;
+    const password=document.getElementById("registerPassword").value;
+    const message=document.getElementById("message");
+    if(!email.includes("@") || !email.includes(".") ||email.indexOf("@")>0 || email.lastIndexOf(".")< email.indexOf("@")+1 || email.lastIndexOf(".")>email.length-1);
+    {
+      message.innerHTML="The email is not invalid";
+      return;
+    }
+    const specialcharacters=/[^a-zA-Z0-9]/.test(username);
+    if(specialcharacters)
+    {
+        message.innerHTML="The username has special characters.";
+        return;
+    }
+    console.log(`Register clicked. Name: ${name}, Email: ${email}, Username: ${username}, Password: ${password}`);
+    const isValid=/(?=.*[A-Z])(?=.*\d)/.test(password);
 
+    if(!name||!email||!username||!password)
+    {
+        message.innerHTML="All fields are requried";
+        return;
+    }
+    if(!isValid)
+    {
+        message.innerHTML="Password follow rules";
+        return;
+    }
+   if(password.length<8)
+   {
+    message.innerHTML="password has atleast 8 characters";
+    return;
+   }
+   message.innerHTML="Successfully Created";
     // Frontend validation for registration form
     
 
@@ -19,4 +63,8 @@ function register() {
     // Validate password (at least 8 characters, one capital letter, and one numeric)
     
 }
-module.exports = { login, register };
+if(typeof module!=='undefined' && module.exports)
+{
+    module.exports = {login,register };
+}
+
